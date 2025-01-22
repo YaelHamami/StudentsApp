8581a2b9b9f050d5760730d29cbd9de4e018d486
Your Name <you@example.com>
2025-01-22 17:24:57 +0200

    add viewbinding

--- app/src/main/java/com/sy/studentsapp/AddStudentActivity.kt
+++ app/src/main/java/com/sy/studentsapp/AddStudentActivity.kt
@@ -5,58 +5,51 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sy.studentsapp.databinding.ActivityAddStudentBinding
+import com.sy.studentsapp.databinding.ActivityMainBinding
 import com.sy.studentsapp.model.Model
 import com.sy.studentsapp.model.Student
 
 class AddStudentActivity : AppCompatActivity() {
-    private var students: List<Student>? = null
+
+    private lateinit var binding: ActivityAddStudentBinding
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize View Binding
+        binding = ActivityAddStudentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_student)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val saveButton: Button = findViewById(R.id.save_button)
-        val cancelButton: Button = findViewById(R.id.cancel_button)
-
-        val nameEditText: EditText = findViewById(R.id.add_student_activity_name_edit_text)
-        val idEditText: EditText = findViewById(R.id.add_student_activity_id_edit_text)
-        val phoneEditText: EditText = findViewById(R.id.add_student_activity_phone_edit_text)
-        val addressEditText: EditText = findViewById(R.id.add_student_activity_address_edit_text)
-
-        val checkedCheckbox: CheckBox = findViewById(R.id.add_student_activity_checked_check_box)
-
-        cancelButton.setOnClickListener{
+        binding.cancelButton.setOnClickListener{
             finish()
         }
 
-        saveButton.setOnClickListener{
+        binding.saveButton.setOnClickListener{
             val student = Student(
-                id = idEditText?.text?.toString() ?: "",
-                name = nameEditText?.text?.toString() ?: "",
-                address = addressEditText?.text?.toString() ?: "",
-                phone = phoneEditText?.text?.toString() ?: "",
-                isChecked = checkedCheckbox?.isChecked ?: false
+                id = binding.idEditText.text?.toString() ?: "",
+                name = binding.nameEditText.text?.toString() ?: "",
+                address = binding.addressEditText.text?.toString() ?: "",
+                phone = binding.phoneEditText.text?.toString() ?: "",
+                isChecked = binding.checkBox.isChecked
             )
 
             Model.shared.add(student) {
                 finish()
             }
-        }
-
-        Model.shared.getAllStudents {
-            this.students = it
         }
     }
 }
